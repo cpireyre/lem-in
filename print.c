9fa6babe0427c26a66dabbeb9737975c23c06be8
Colin PIREYRE <cpireyre@student.42.fr>
2018-11-09 08:05:28 +0100

    debug print now gives room type

--- print.c
+++ print.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 08:20:47 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/11/08 11:25:23 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/11/09 08:03:20 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,20 @@ void	print_list(t_list **head)
 void	print_rooms(t_rooms *map)
 {
 	int		i;
+	char	*room_type;
 
 	i = -1;
 	while (map)
 	{
+		if (map->type == END)
+			room_type = "END";
+		else if (map->type == START)
+			room_type = "START";
+		else
+			room_type = "NORMAL";
 		if (DEBUG)
 			ft_putstr("DEBUG: ");
-		ft_printf("Room %d: %s, type %hhu.\n", ++i, map->name, map->type);
+		ft_printf("Room %d: %s, type %s.\n", ++i, map->name, room_type);
 		map = map->next;
 	}
 }
