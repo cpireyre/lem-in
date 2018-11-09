97541332476650f43ec00ae3c513b84cf7e9b4ba
Colin PIREYRE <cpireyre@student.42.fr>
2018-11-09 15:08:37 +0100

    fix

--- print.c
+++ print.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 08:20:47 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/11/09 14:53:55 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/11/09 15:08:11 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,6 @@ void	print_list(t_list **head)
 	}
 }
 
-int		tablen(int *tab)
-{
-	return (*tab ? 1 + tablen(++tab) : 0);
-}
 
 void	print_paths(t_paths *path, int len)
 {
@@ -45,7 +41,6 @@ void	print_paths(t_paths *path, int len)
 	ft_putendl("And backwards:");
 	while (path)
 	{
-		len = tablen(path->scout);
 		ft_print_int_tab(path->scout, len);
 		path = path->prev;
 	}
