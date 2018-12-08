1cbcd46241e3fd00cefc6da7823b2b09aaf47c5c
cpireyre <cpireyre@student.42.fr>
2018-12-08 13:36:28 +0100

    removed archaic comments

--- src/super.c
+++ src/super.c
@@ -18,35 +18,6 @@ static int		flow_to_vertex(t_list *vertex)
 	return (count);
 }
 
-/* t_edge			*edge_to_preserve(t_listarray graph, int start_id, int vtx)
-{
-	t_list	*vertex;
-	int		min_path_length;
-	t_edge	*edge;
-	int		min_tmp;
-	t_edge	*shortest_path;
-
-	vertex = (graph)[vtx];
-	min_path_length = 2147483647;
-	while (vertex)
-	{
-		edge = ((t_edge*)(vertex->content))->rev;
-		if (edge->flow == 1)
-		{
-			min_tmp = 1 + count_back_length((graph), edge->source, start_id);
-			if (min_tmp < min_path_length)
-			{
-				min_path_length = min_tmp;
-				shortest_path = edge;
-			}
-		}
-		vertex = vertex->next;
-	}
-	if (DEBUG)
-		ft_printf("\tDEBUG: Shortest path goes %d->%d.\n", shortest_path->source, shortest_path->sink);
-	return (edge);
-} */
-
 void			zero_path(t_list *vertex, t_listarray graph, int sink)
 {
 	t_edge	*edge;
@@ -69,27 +40,6 @@ void			zero_path(t_list *vertex, t_listarray graph, int sink)
 		vertex = graph[edge->sink];
 	}
 }
-
-/* void			flow_zero(t_listarray graph, t_edge *leave_alone, int vtx, int start_id)
-{
-	t_list	*vertex;
-	t_edge	*edge;
-
-	if (DEBUG)
-		ft_printf("DEBUG: Clearing superposed paths around vertex %d.\n", vtx);
-	vertex = (graph)[vtx];
-	while (vertex)
-	{
-		edge = ((t_edge*)(vertex->content))->rev;
-		if (edge->flow == 1 && edge != leave_alone)
-		{
-			if (DEBUG)
-				ft_printf("\t...found one.\n");
-			zero_path(vtx, start_id, graph);
-		}
-		vertex = vertex->next;
-	}
-} */
 
 t_bool			path_is_super(t_listarray graph, int end_id, t_list *vertex)
 {
